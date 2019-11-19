d3b05935334b86902054a4db4e32969fd88b274e
Alfred <alfredleo@gmail.com>
2019-11-19 06:41:29 +0500

    7. Keys

---
+++ src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        <div><slot></slot> {{task.id}}</div>
+        <small>{{task.dueDate}} @ {{task.dueTime}}</small>
+        <button @click="deleteTask(index)">X</button>
+    </li>
+</template> 
+
+<script>
+    export default {
+        props: ['task', 'index']
+    }
+</script>
+
+<style>
+
+</style>
