a60c4ea61c88ff973636aff6c62173ef2be9bbbc
Alfred <alfredleo@gmail.com>
2019-11-21 04:35:34 +0500

    6.2 Display a List of Tasks

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,12 +1,51 @@
 <template>
-  <q-page padding>
-    <p>Todo page</p>
+  <q-page class="q-pa-lg">
+    <q-list bordered>
+      <q-item-label header>General</q-item-label>
+
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
+        v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{task.name}}</q-item-label>
+        </q-item-section>
+      </q-item>
+      
+    </q-list>
   </q-page>
 </template> 
 
 <script>
 export default {
-}
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: "Go to shop",
+          completed: false
+        },
+        {
+          id: 2,
+          name: "Get bananas",
+          completed: true
+        },
+        {
+          id: 3,
+          name: "Get apples",
+          completed: false
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
