b9509cd02560f9474ce8030336bc25015083a34a
Alfred <alfredleo@gmail.com>
2019-11-21 05:05:26 +0500

    6.4 Add some Style to the List of Tasks

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,12 +1,11 @@
 <template>
   <q-page class="q-pa-lg">
-    <q-list bordered>
-      <q-item-label header>General</q-item-label>
-
+    <q-list bordered separator>
       <q-item
         v-for="task in tasks"
         :key="task.id"
         @click="task.completed = !task.completed"
+        :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
         clickable
         v-ripple
       >
@@ -15,17 +14,19 @@
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>{{task.name}}</q-item-label>
+          <q-item-label :class="{'text-strikethrough': task.completed}">{{task.name}}</q-item-label>
         </q-item-section>
 
         <q-item-section side>
           <div class="row">
             <div class="column justify-center">
-              <q-icon name="event" size="18px" class="q-mr-xs"/>
+              <q-icon name="event" size="18px" class="q-mr-xs" />
             </div>
             <div class="column">
               <q-item-label class="row justify-end" caption>{{task.dueDate}}</q-item-label>
-              <q-item-label class="row justify-end" caption><small>{{task.dueTime}}</small></q-item-label>
+              <q-item-label class="row justify-end" caption>
+                <small>{{task.dueTime}}</small>
+              </q-item-label>
             </div>
           </div>
         </q-item-section>
