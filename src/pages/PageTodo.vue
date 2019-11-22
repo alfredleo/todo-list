5f6111c7342470b766ac78ea89f9ff7d93238e68
Alfred <alfredleo@gmail.com>
2019-11-23 02:56:47 +0500

    9.2 Add-task component is created.

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="q-pa-lg">
+  <q-page class="q-pa-md">
     <q-list bordered separator>
       <task
         v-for="(task,key) in tasks"
@@ -7,17 +7,39 @@
         :task="task"
         :id="key"></task>
     </q-list>
+    <div class="absolute-bottom text-center q-ma-lg">
+      <q-btn
+        @click="showAddTask = true"
+        :key="55"
+        glossy round
+        color="primary"
+        size="24px"
+        icon="add"
+      />
+    </div>
+
+    <q-dialog v-model="showAddTask">
+      <add-task/>
+    </q-dialog>
+
   </q-page>
 </template>
 
 <script>
     import {mapGetters} from "vuex";
+    import AddTask from "../components/Tasks/Modals/AddTask";
 
     export default {
+        data() {
+            return {
+                showAddTask: false
+            }
+        },
         computed: {
             ...mapGetters("tasks", ["tasks"])
         },
         components: {
+            AddTask,
             task: require("components/Tasks/Task.vue").default
         }
     };
